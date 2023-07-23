2b86839feb0ba984f609a616c467437854fe65ac
Shaker Hossain <73550448+shaker87@users.noreply.github.com>
2023-07-23 20:44:57 +0600

    Sign in with facebook, custom email and google

--- src/compnents/Auth/SignIn.jsx
+++ src/compnents/Auth/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { auth, db } from "../../firebase/init";
-import { FacebookAuthProvider, GoogleAuthProvider, OAuthProvider , getRedirectResult, signInWithRedirect } from "firebase/auth";
+import './signIn.css'
+import { auth, db, facebookAuth, googleAuth } from "../../firebase/init";
+import { FacebookAuthProvider, GoogleAuthProvider, OAuthProvider , getRedirectResult, isSignInWithEmailLink, sendSignInLinkToEmail, signInWithEmailLink, signInWithPopup, signInWithRedirect } from "firebase/auth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Operation from "../../ui/Operation";
 import { doc, updateDoc } from "firebase/firestore";
@@ -9,7 +10,12 @@ function SignIn({ setUser , usersList }) {
 
   const [operation , setOperation] = useState(false)
   const [operationState , setOperationState] = useState(true)
-  const [message , setMessage] = useState('')
+  const [message , setMessage] = useState('');
+
+  const [isOpenEmailInput, setIsOpenEmailInput] = useState(false)
+  const [email, setEmail] = useState('')
+  const [isSentEmail, setIsSentEmail] = useState(false);
+
 
   function displayOperation(message , state){
     setOperationState(state)
@@ -25,21 +31,29 @@ function SignIn({ setUser , usersList }) {
     }
   }, [operation])
 
-  let provider 
+  async function handleSendEmailInvitation(e){
+    e.preventDefault()
+    sendSignInLinkToEmail(auth, email, {
+      url: 'http://localhost:3000/signin',
+      handleCodeInApp: true
+    }).then((res)=> {
+      localStorage.setItem('email', email);
+      setIsSentEmail(true)
 
-  async function signIn(providerType) {
-    if(providerType === "google"){
-      provider = new GoogleAuthProvider()
-      provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    }else if(providerType === "apple"){
-      provider = new OAuthProvider('apple.com')
-    }else if(providerType === 'facebook'){
-      provider= new FacebookAuthProvider()
-      provider.addScope('user_birthday');
-    }
+    })
+  }
 
-    await signInWithRedirect(auth, provider)
+
+
+  async function signInWithGoogle() {
+    const user = await googleAuth();
+    console.log('user', user)
+
   };
+  async function signInWithFacebook(){
+    const user = await facebookAuth();
+    console.log('user', user)
+  }
 
   useEffect(() => {
     getRedirectResult(auth)
@@ -53,6 +67,18 @@ function SignIn({ setUser , usersList }) {
         .catch(() => {
           
         })
+
+        if(isSignInWithEmailLink(auth, window.location.href)){
+          const email = localStorage.getItem('email');
+         
+          signInWithEmailLink(auth, localStorage.getItem('email'), window.location.href)
+          .then((res)=> {
+            if(res.user){
+              
+            }
+            localStorage.removeItem('email')
+          })
+        }
   },[])
 
   return (
@@ -68,10 +94,31 @@ function SignIn({ setUser , usersList }) {
           <p className="block--note">hey there !</p>
           <h3 className="block--header">Welcome to rhycon</h3>
             <div className="block--providers">
-              <button onClick={() => signIn('google')} className="block__provider"><FontAwesomeIcon className="block__provider--logo google" icon='fa-brands fa-google'/> Continue with Google</button>
-              <button onClick={() => signIn('facebook')} className="block__provider"><FontAwesomeIcon className="block__provider--logo facebook" icon='fa-brands fa-facebook'/> Continue with Facebook</button>
+              <button onClick={() => signInWithGoogle()} className="block__provider"><FontAwesomeIcon className="block__provider--logo google" icon='fa-brands fa-google'/> Continue with Google</button>
+              <button onClick={() => signInWithFacebook()} className="block__provider"><FontAwesomeIcon className="block__provider--logo facebook" icon='fa-brands fa-facebook'/> Continue with Facebook</button>
+
+              {
+                isOpenEmailInput === true? 
+
+                isSentEmail ? <p style={{color:'green'}}>We sent sign in link on your email.</p> 
+                :
+                <form onSubmit={handleSendEmailInvitation} class="input-container">
+                <input onChange={(e)=> setEmail(e.target.value)} value={email} type="text" placeholder="Enter your email here" />
+                <button><FontAwesomeIcon className="block__provider--logo facebook" icon="fa-solid fa-arrow-right" /></button>
+              </form>
+              :
+          
+               
+              <button onClick={() => setIsOpenEmailInput(true)} className="block__provider"><FontAwesomeIcon className="block__provider--logo email" icon="fa-solid fa-envelope" /> Continue with Email Invitation</button>
+              }
+
             </div>
+            
         </div>
+
+        
+       
+   
       </div>
       {
         operation && <Operation success={operationState} message={message} setOperation={setOperation} />
