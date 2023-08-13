bf72bc40ee99c1809cb37d409ce2344762872c11
Rhycon-Trades <139068293+Rhycon-Trades@users.noreply.github.com>
2023-08-13 21:36:52 +0100

    Update AboutProduct.jsx

--- src/pages/AboutProduct.jsx
+++ src/pages/AboutProduct.jsx
@@ -229,7 +229,7 @@ function AboutProduct({ products, cart , user }) {
           <div className="gallery">
             <figure className="gallery--img">
               {gallery === 1 && <img className="skeleton-img" src="https://media.discordapp.net/attachments/1045402320252444693/1094942562801954846/unnamed_2.jpg?width=604&height=604"/> }
-              {gallery === 2 && <img className="skeleton-img" src="https://media.discordapp.net/attachments/1045402320252444693/1094951282105651220/Untitled_design_16.png?width=604&height=604" alt="" /> }
+              {gallery === 2 && <img className="skeleton-img" src="https://firebasestorage.googleapis.com/v0/b/rhycon-trades.appspot.com/o/images%2Fa501269d-ca50-43c1-bf47-d0a5b2f836c4?alt=media&token=d4b5784d-253d-4b7d-a25c-69f4d311c946" alt="" /> }
               {gallery === 3 && <img className="skeleton-img" src="https://cdn.discordapp.com/attachments/1075874020299395134/1136963102475305070/IMG_1641.jpg" alt="" /> }
             </figure>
             <div className="testimonials--arrows">
