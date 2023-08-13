ee891b8a910fc100ebcf5a7392e78a1f5616f0ad
Rhycon-Trades <139068293+Rhycon-Trades@users.noreply.github.com>
2023-08-13 19:49:38 +0100

    Update Trusted.jsx

--- src/compnents/Trusted.jsx
+++ src/compnents/Trusted.jsx
@@ -5,28 +5,4 @@ import tradingview from '../img/tradingview.svg'
 import forbes from '../img/forbes.png'
 
 
-function Trusted() {
-  return (
-    <section id="trusted">
-        <div className="trusted-container">
-            <h6>Trusted By Thousands Of People</h6>
-            <ul className="trusted__logos">
-                <li className="trusted__logo">
-                    <img src={stripe} />
-                </li>
-                <li className="trusted__logo">
-                    <img src={discord} />
-                </li>
-                <li className="trusted__logo">
-                    <img src={tradingview} />
-                </li>
-                <li className="trusted__logo">
-                    <img src={forbes} />
-                </li>
-            </ul>
-        </div>
-    </section>
-  )
-}
-
 export default Trusted
